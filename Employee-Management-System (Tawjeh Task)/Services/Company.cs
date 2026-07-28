5e81c8f43b0bc7563e2fbc19cf99ebd132bbf9be
Mohammed Hamdy <mohammedhamdyelnagar@gmail.com>
2026-07-28 14:45:35 +0300

    feat: add company collections

---
+++ Employee-Management-System (Tawjeh Task)/Services/Company.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Employee_Management_System__Tawjeh_Task_.Models;
+
+namespace Employee_Management_System__Tawjeh_Task_.Services
+{
+    public class Company
+    {
+        private List<Employee> ActiveEmployees = new List<Employee> ();
+        private Dictionary<int, Department> Departments = new Dictionary<int, Department> ();
+        private Queue<Employee> onboardingEmployees = new Queue<Employee> ();
+        private Stack<string> ActionHistory = new Stack<string> ();
+        private HashSet<string> Skills = new HashSet<string> ();
+
+        public Company ()
+        {
+            //Seed Data
+        }
+    }
+}
